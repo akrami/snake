71e011577908ff0cfe5984ed59b30ae25da7be2e
Alireza Akrami <akrami.ui@gmail.com>
2019-04-26 14:46:40 +0430

    pause fixed

--- app-client.js
+++ app-client.js
@@ -6,19 +6,15 @@ const w = 20,
     o = 2;
 
 let snake = [];
-snake[0] = {
-    i: 10,
-    j: 10
-};
+snake[0] = { i: 10, j: 10 };
 let food = {
     i: Math.floor(Math.random() * 20),
     j: Math.floor(Math.random() * 20)
 };
 let score = 0;
-let move = {
-    i: 0,
-    j: 0
-};
+let move = { i: 0, j: 0 };
+let direction = "RIGHT";
+let pause = false;
 let speed = 300;
 let lost = false;
 let lock = false;
@@ -31,39 +27,39 @@ var draw = () => {
         ctx.fillStyle = '#7cad51';
         ctx.fillRect(20, 20, 400, 400);
         ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(440,0);
-        ctx.lineTo(430,10);
-        ctx.lineTo(10,10);
-        ctx.lineTo(10,530);
-        ctx.lineTo(0,540);
+        ctx.moveTo(0, 0);
+        ctx.lineTo(440, 0);
+        ctx.lineTo(430, 10);
+        ctx.lineTo(10, 10);
+        ctx.lineTo(10, 530);
+        ctx.lineTo(0, 540);
         ctx.fillStyle = 'rgba(255,255,255,0.2)';
         ctx.fill();
         ctx.beginPath();
-        ctx.moveTo(420,420);
-        ctx.lineTo(20,420);
-        ctx.lineTo(25,415);
-        ctx.lineTo(415,415);
-        ctx.lineTo(415,25);
-        ctx.lineTo(420,20);
+        ctx.moveTo(420, 420);
+        ctx.lineTo(20, 420);
+        ctx.lineTo(25, 415);
+        ctx.lineTo(415, 415);
+        ctx.lineTo(415, 25);
+        ctx.lineTo(420, 20);
         ctx.fillStyle = 'rgba(255,255,255,0.2)';
         ctx.fill();
         ctx.beginPath();
-        ctx.moveTo(440,540);
-        ctx.lineTo(440,0);
-        ctx.lineTo(430,10);
-        ctx.lineTo(430,530);
-        ctx.lineTo(10,530);
-        ctx.lineTo(0,540);
+        ctx.moveTo(440, 540);
+        ctx.lineTo(440, 0);
+        ctx.lineTo(430, 10);
+        ctx.lineTo(430, 530);
+        ctx.lineTo(10, 530);
+        ctx.lineTo(0, 540);
         ctx.fillStyle = 'rgba(0,0,0,0.2)';
         ctx.fill();
         ctx.beginPath();
-        ctx.moveTo(20,20);
-        ctx.lineTo(420,20);
-        ctx.lineTo(415,25);
-        ctx.lineTo(25,25);
-        ctx.lineTo(25,415);
-        ctx.lineTo(20,420);
+        ctx.moveTo(20, 20);
+        ctx.lineTo(420, 20);
+        ctx.lineTo(415, 25);
+        ctx.lineTo(25, 25);
+        ctx.lineTo(25, 415);
+        ctx.lineTo(20, 420);
         ctx.fillStyle = 'rgba(0,0,0,0.2)';
         ctx.fill();
 
@@ -116,71 +112,41 @@ var draw = () => {
     }
 }
 
-let direction = "RIGHT";
-let pause = false;
 document.addEventListener("keydown", (event) => {
     if (event.keyCode == 37 && direction != "RIGHT" && !pause && !lost && !lock) {
         lock = true;
         direction = "LEFT";
-        move = {
-            i: -1,
-            j: 0
-        };
+        move = { i: -1, j: 0 };
     } else if (event.keyCode == 38 && direction != "DOWN" && !pause && !lost && !lock) {
         lock = true;
         direction = "UP";
-        move = {
-            i: 0,
-            j: -1
-        };
+        move = { i: 0, j: -1 };
     } else if (event.keyCode == 39 && direction != "LEFT" && !pause && !lost && !lock) {
         lock = true;
         direction = "RIGHT";
-        move = {
-            i: 1,
-            j: 0
-        };
+        move = { i: 1, j: 0 };
     } else if (event.keyCode == 40 && direction != "UP" && !pause && !lost && !lock) {
         lock = true;
         direction = "DOWN";
-        move = {
-            i: 0,
-            j: 1
-        };
+        move = { i: 0, j: 1 };
     } else if (event.keyCode == 32 && !pause && !lost && !lock) {
-        lock = true;
         pause = true;
-        move = {
-            i: 0,
-            j: 0
-        };
+        move = { i: 0, j: 0 };
     } else if (event.keyCode == 32 && pause && !lost && !lock) {
         lock = true;
         pause = false;
         switch (direction) {
             case 'LEFT':
-                move = {
-                    i: -1,
-                    j: 0
-                };
+                move = { i: -1, j: 0 };
                 break;
             case 'UP':
-                move = {
-                    i: 0,
-                    j: -1
-                };
+                move = { i: 0, j: -1 };
                 break;
             case 'RIGHT':
-                move = {
-                    i: 1,
-                    j: 0
-                };
+                move = { i: 1, j: 0 };
                 break;
             case 'DOWN':
-                move = {
-                    i: 0,
-                    j: 1
-                };
+                move = { i: 0, j: 1 };
                 break;
         }
     }
